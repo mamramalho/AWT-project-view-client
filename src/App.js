b6ca7afd5ab595f4b947b0d568efa6ad5a6be14b
Miguel Ramalho <mamramalho@Miguels-MacBook.local>
2023-04-14 09:40:57 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [listOfEvents, setListOfEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/event").then((response) => {
+      setListOfEvents(response.data);
+    });
+  });
+
+  return (
+    <div className="App">
+      {listOfEvents.map((value, key) => {
+        return (
+          <div className="events">
+            <div className="title"> {value.title} </div>
+            <div className="description"> {value.description} </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
