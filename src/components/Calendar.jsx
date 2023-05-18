20a60f4fbb264a4c7b4a56bad025542bfc39bccf
Miguel Ramalho <mamramalho@Miguels-MacBook.local>
2023-05-18 21:36:04 +0200

    Changes

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactCalendar from "react-calendar";
 import "./CalendarStyles.css";
 
 const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    console.log("Events for date:", date);
+  };
+
   return (
     <div className="container">
       <ReactCalendar
         minDate={new Date()}
         className="REACT-CALENDAR"
         view="month"
-        onClickDay={(date) => console.log(date)}
+        onClickDay={handleDateClick}
       />
+      {selectedDate && (
+        <div className="selected-events">
+          <h2>Events for {selectedDate.toDateString()}</h2>
+          {/* Display the events for the selected date */}
+          {/* You can use the selectedDate to filter the events */}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import "./EventsStyles.css"
+import "./EventsStyles.css";
 
 function Events() {
   const [listOfEvents, setListOfEvents] = useState([]);
@@ -10,17 +9,65 @@ function Events() {
     axios.get("http://localhost:3001/event").then((response) => {
       setListOfEvents(response.data);
     });
-  });
+  }, []);
+
+  const handleEdit = (event) => {
+    const eventId = event.target.dataset.eventId;
+    // Open a window or modal with the event details for editing
+    console.log("Edit event with ID:", eventId);
+  };
+
+  const handleInvite = (event) => {
+    const eventId = event.target.dataset.eventId;
+    // Open a window or modal with the invitation form for the event
+    console.log("Invite people to event with ID:", eventId);
+  };
+
+  const handleDelete = (event) => {
+    const eventId = event.target.dataset.eventId;
+    // Delete the event from the user's account
+    console.log("Delete event with ID:", eventId);
+  };
 
   return (
-    <div className="App">
+    <div className="event-container">
+      <div className="new">
+        <button className="newEvent" type="submit">
+          Create Event
+        </button>
+      </div>
       <ul>
-        {listOfEvents.map((value, key) => {
+        {listOfEvents.map((value, index) => {
           return (
-            <div className="events">
-              <div className="title">{value.title}</div>
-              <button className="delete" type="submit">Delete</button>
-              <div className="description">{value.description}</div>
+            <div className="event" key={index}>
+              <div className="optionsEvent">
+                <button
+                  className="editEvent"
+                  type="button"
+                  data-event-id={value.id}
+                  onClick={handleEdit}
+                >
+                  Edit
+                </button>
+                <button
+                  className="inviteEvent"
+                  type="button"
+                  data-event-id={value.id}
+                  onClick={handleInvite}
+                >
+                  Invite
+                </button>
+                <button
+                  className="deleteEvent"
+                  type="button"
+                  data-event-id={value.id}
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+              </div>
+              <div className="titleEvent">{value.title}</div>
+              <div className="descriptionEvent">{value.description}</div>
             </div>
           );
         })}
